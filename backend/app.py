9f14d8bc6f2155f85e9fc7ab1130e756bf701f50
kioenguyen0207 <nguyendinhquy2000@gmail.com>
2022-09-29 09:46:47 +0700

    init commit

---
+++ backend/app.py
@@ -0,0 +1,91 @@
+import json
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource, reqparse
+import werkzeug
+import cv2
+import numpy as np
+from triip_detect import detect
+import os
+from dotenv import load_dotenv
+from flask_cors import CORS
+
+from s3_controller import s3_upload
+from db_controller import importData, readPendingReq, readAllReq, readRejectedReq, readApprovedReq, readById
+
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+
+load_dotenv()
+ACCESS_POINT = os.getenv('ACCESS_POINT')
+
+class Home(Resource):
+    def get(self):
+        try:
+            return {'server_status': 'OK'}, 200
+        except Exception as ex:
+            return {'msg': "Something's happened",
+                    'error details': ex}, 500
+
+class send_kyc_request(Resource):
+    def post(self):
+        try:
+            parser = reqparse.RequestParser(bundle_errors=True)
+            parser.add_argument('file', type=werkzeug.datastructures.FileStorage, location='files', required=True)
+            parser.add_argument('user_id', location='form', required=True)
+            parser.add_argument('username', location='form', required=True)
+            parser.add_argument('address', location='form', required=True)
+            parser.add_argument('phone', location='form', required=True)
+            args = parser.parse_args()
+            uploaded_image = args['file'].read()
+            img = cv2.imdecode(np.frombuffer(uploaded_image, np.uint8), cv2.IMREAD_COLOR)
+            result = detect(img)
+            detectedElements = []
+            for key in result:
+                p = result[key]
+                img = cv2.rectangle(img, (p[0], p[1]) , (p[2], p[3]), (255, 0, 0), 2)
+                detectedElements.append(key)
+            s3_upload(img, args['user_id'] + '.png')
+            importData(args['user_id'], args['username'], args['address'], args['phone'], ACCESS_POINT + args['user_id'] + '.png', detectedElements)
+            return {
+                'Status': 'Success!'
+            }
+        except Exception as ex:
+            print(ex)
+            return {
+                'msg': "Something's happened",
+                'description': str(ex)
+                }, 500
+
+class getAllRequest(Resource):
+    def get(self):
+        return jsonify(readAllReq())
+
+class getRejectedRequest(Resource):
+    def get(self):
+        return jsonify(readRejectedReq())
+
+class getPendingRequest(Resource):
+    def get(self):
+        return jsonify(readPendingReq())
+
+class getApprovedRequest(Resource):
+    def get(self):
+        return jsonify(readApprovedReq())
+
+class getById(Resource):
+    def get(self):
+        id = request.args.get('id')
+        return jsonify(readById(int(id))[0])
+
+#endpoint(s)
+api.add_resource(Home, "/")
+api.add_resource(send_kyc_request, "/kyc")
+api.add_resource(getAllRequest, "/get/all")
+api.add_resource(getRejectedRequest, "/get/rejected")
+api.add_resource(getPendingRequest, "/get/pending")
+api.add_resource(getApprovedRequest, "/get/approved")
+api.add_resource(getById, "/get")
+
+if __name__=='__main__':
+    app.run(host="0.0.0.0", port=5050)
---
+++ backend/db_controller.py
@@ -0,0 +1,75 @@
+from boto3 import resource
+from boto3.dynamodb.conditions import Key, Attr
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+import random
+
+load_dotenv()
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+REGION_NAME = os.getenv('REGION_NAME')
+
+resource = resource(
+   'dynamodb',
+   aws_access_key_id     = AWS_ACCESS_KEY_ID,
+   aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
+   region_name           = REGION_NAME
+)
+
+kyc_requests_table = resource.Table('kyc_requests')
+
+def importData(user_id, username, address, phone, image_path, detected_objects, time = datetime.now().strftime("%d/%m/%Y %H:%M:%S")):
+    response = kyc_requests_table.put_item(
+        Item = {
+            'request_id': random.randint(100000, 999999),
+            'user_id': user_id,
+            'username': username,
+            'address' : address,
+            'phone'  : phone,
+            'createdAt': time,
+            'updatedAt': time,
+            'image': image_path,
+            'detected_objects': detected_objects,
+            'comment': 'null',
+            'approver': 'admin',
+            'kycStatus': 'pending'
+        }
+    )
+    print(response)
+    return response
+
+def readPendingReq():
+    response = kyc_requests_table.scan(
+        FilterExpression = Attr("kycStatus").eq('pending')
+    )
+    return response['Items']
+
+def readAllReq():
+    response = kyc_requests_table.scan()
+    return response['Items']
+
+def readRejectedReq():
+    response = kyc_requests_table.scan(
+        FilterExpression = Attr("kycStatus").eq('rejected')
+    )
+    return response['Items']
+
+def readApprovedReq():
+    response = kyc_requests_table.scan(
+        FilterExpression = Attr("kycStatus").eq('approved')
+    )
+    return response['Items']
+
+def readById(id):
+    response = kyc_requests_table.query(
+        KeyConditionExpression=Key('request_id').eq(id)
+    )
+    return response['Items']
+
+if __name__ == '__main__':
+    # for item in readAllReq():
+    #     print('------')
+    #     print(item)
+
+    print(readById(69))
